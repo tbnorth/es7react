f091c61fa140e3eb1690cc721fe22cfb3a713e64
terrynbrown@gmail.com <terrynbrown@gmail.com>
2017-09-29 16:56:44 -0500

    select element reacting to addition of items

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RaisedButton from 'material-ui/RaisedButton';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
@@ -13,32 +15,41 @@ ReactDOM.render(
 
 class TestApp extends React.Component {
 
-    constructor(prop) {
-        super(prop)
+    constructor() {
+        super()
+        this.state = {
+            value: '1_0',
+            things: [1, 'two', 'iii']
+        }
     }
 
     getComponent = () => {
         console.log(this.testES7())
     }
 
+    handleChange = (event, index, value) => this.setState({value});  // short for {value: value}
+
     render = () => {
         let theme = getMuiTheme();
+        let items = this.state.things.map( (x,n) => {
+            let key = `${x}_${n}`
+            return(<MenuItem key={n} value={key} primaryText={key}/>)
+        })
         return(
-          <MuiThemeProvider muiTheme={theme}>
-            <RaisedButton label='test' onClick={this.getComponent}/>
-          </MuiThemeProvider>
+          <MuiThemeProvider muiTheme={theme}><div>
+            <RaisedButton label='test' onClick={this.getComponent}/><br/>
+            <SelectField floatingLabelText="Frequency" value={this.state.value}
+              onChange={this.handleChange}>{items}</SelectField>
+          </div></MuiThemeProvider>
         )
     }
 
-    testES7 = () => "Must be ES7"
+    testES7 = () => {
+        let added = {...this.state}
+        added.things.push('otters')
+        this.setState(added)
+        return `Must be ES7: ${this.state.value}`
+    }
 }
 
-ReactDOM.render(<TestApp />, document.getElementById('content'))
-
-// let theme = getMuiTheme();
-// ReactDOM.render(
-// <MuiThemeProvider muiTheme={theme}>
-// <RaisedButton label='test'/>
-// </MuiThemeProvider>
-// , document.getElementById('content'))
-
+ReactDOM.render(<TestApp/>, document.getElementById('content'))
