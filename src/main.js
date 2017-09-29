424dabbbe48f18af2555ddd2d937b571bc1e0efe
terrynbrown@gmail.com <terrynbrown@gmail.com>
2017-09-29 15:31:25 -0500

    on-click

--- src/main.js
+++ src/main.js
@@ -5,5 +5,27 @@ console.log("Start")
 
 ReactDOM.render(
   <h1>Hello, world!</h1>,
-  document.getElementById('root')
+  document.getElementById('header')
 )
+
+class TestApp extends  React.Component {
+    getComponent = () => {
+        console.log(this.testES7())
+    }
+
+    render() {
+        return(
+          <div>
+            <ul>
+              <li onClick={this.getComponent}>Component 1</li>
+            </ul>
+          </div>
+        )
+    }
+
+    testES7 = () => "Must be ES7"
+
+}
+
+ReactDOM.render(<TestApp />, document.getElementById('content'))
+
